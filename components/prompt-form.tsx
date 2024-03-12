49e9670e39fd7d83691a587d5d9e0caeb8508529
Sam Stenner <sam.stenner2@gmail.com>
2024-03-12 17:38:26 +0000

    fix: styling

--- components/prompt-form.tsx
+++ components/prompt-form.tsx
@@ -48,26 +48,7 @@ export function PromptForm({
       }}
       ref={formRef}
     >
-      <div className="relative flex flex-col w-full px-2 overflow-hidden max-h-60 grow sm:rounded-md sm:px-3">
-        {/* <Tooltip>
-          <TooltipTrigger asChild>
-            <button
-              onClick={e => {
-                e.preventDefault()
-                router.refresh()
-                router.push('/')
-              }}
-              className={cn(
-                buttonVariants({ size: 'sm', variant: 'outline' }),
-                'absolute left-0 top-4 size-8 rounded-full bg-background p-0 sm:left-4'
-              )}
-            >
-              <IconPlus />
-              <span className="sr-only">New Chat</span>
-            </button>
-          </TooltipTrigger>
-          <TooltipContent>New Chat</TooltipContent>
-        </Tooltip> */}
+      <div className="relative flex flex-col w-full pr-8 overflow-hidden max-h-60 grow sm:pr-12">
         <Textarea
           ref={inputRef}
           disabled={disabled}
@@ -80,7 +61,7 @@ export function PromptForm({
           spellCheck={false}
           className="min-h-[60px] w-full resize-none bg-transparent px-4 py-[1.3rem] focus-within:outline-none sm:text-sm"
         />
-        <div className="absolute right-0 top-4 sm:right-4">
+        <div className="absolute right-0 bottom-3 sm:right-4">
           <Tooltip>
             <TooltipTrigger asChild>
               {isLoading ? 
